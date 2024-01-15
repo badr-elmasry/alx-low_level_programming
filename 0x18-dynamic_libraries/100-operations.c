461ce690fd6a7366df600a9cadc9226cccc16781
badr-elmasry <badr.ck.elmasry@gmail.com>
2024-01-15 03:07:09 -0800

    push

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * add - returns the sum of two numbers
+ * @x: the augend
+ * @y: the addend
+ *
+ * Author: Badr
+ * Return: the sum
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
+
+/**
+ * sub - returns the difference of two numbers
+ * @x: the minuend
+ * @y: the subtrahend
+ *
+ * Author: Badr
+ * Return: the difference
+ */
+int sub(int x, int y)
+{
+	return (x - y);
+}
+
+/**
+ * mul - returns the product of two numbers
+ * @x: the multiplicand
+ * @y: the multiplier
+ *
+ * Author: Badr
+ * Return: the product
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
+
+/**
+ * div - returns the quotient of two numbers
+ * @x: the dividend
+ * @y: the divisor
+ *
+ * Author: Badr
+ * Return: the quotient
+ */
+int div(int x, int y)
+{
+	return (x / y);
+}
+
+/**
+ * mod - returns the remainder of two numbers
+ * @x: the dividend
+ * @y: the divisor
+ *
+ * Author: Badr
+ * Return: the remainder
+ */
+int mod(int x, int y)
+{
+	return (x % y);
+}
